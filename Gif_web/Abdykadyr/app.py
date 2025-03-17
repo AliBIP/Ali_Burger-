7e4e87ed38c28511202dafccf621504f0a17b96f
Abdykadyr66 <abdykadyr.turduev@alatoo.edu.kg>
2025-03-17 11:57:14 +0600

    Asds

--- Gif_web/Abdykadyr/app.py
+++ Gif_web/Abdykadyr/app.py
@@ -1,18 +1,25 @@
 import pandas as pd
 from flask import Flask, render_template, request
 
+# Чтение CSV файла с подарками
 gifts_df = pd.read_csv('gifts_data.csv')
+
+# Инициализация Flask приложения
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    # Получаем список категорий из данных
     categories = gifts_df['category'].unique()
+    
+    # По умолчанию показываем все подарки
     return render_template('index.html', gifts=gifts_df.to_dict(orient='records'), categories=categories)
 
 @app.route('/filter', methods=['GET'])
 def filter_gifts():
     category = request.args.get('category', default=None)
 
+    # Если категория выбрана, фильтруем подарки
     if category:
         filtered_gifts = gifts_df[gifts_df['category'] == category]
     else:
