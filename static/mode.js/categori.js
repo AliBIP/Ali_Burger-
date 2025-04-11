4dde3fec8cca0b38589c1c5af79a9a62d9bc939e
Ali <ali@example.com>
2025-04-11 22:27:12 +0600

    Hello

---
+++ static/mode.js/categori.js
@@ -0,0 +1,23 @@
+
+const sliderContainer = document.querySelector('.slider-container');
+const slides = document.querySelectorAll('.slide');
+const dots = document.querySelectorAll('.slider-dot');
+let currentSlide = 0;
+
+function showSlide(index) {
+  sliderContainer.style.transform = `translateX(-${index * 100}%)`;
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[index].classList.add('active');
+}
+
+setInterval(() => {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}, 5000);
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    currentSlide = index;
+    showSlide(currentSlide);
+  });
+});
